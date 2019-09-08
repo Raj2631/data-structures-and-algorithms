7435038fa51089c092c9275ba4ca1b5760187b3e
Rajat <43057108+Raj2631@users.noreply.github.com>
2019-09-08 08:09:17 +0530

    Update merge_sort.cpp

--- merge_sort.cpp
+++ merge_sort.cpp
@@ -25,17 +25,20 @@ int main(void)
 }
 
 /*
-for this function, 5 parameters are passed, a[] is the array where we are to merge the 2 sub arrays, left[] and right[], nl tells us the number of elements present in the left subarray, while nr tells us the number of elements present in the right subarray.
+for this function, 5 parameters are passed, a[] is the array where we are to merge the 2 sub arrays, left[] and right[], 
+nl tells us the number of elements present in the left subarray, while nr tells us the number of elements present in the right subarray.
 */
 void merge(int a[], int left[], int right[], int nl, int nr)
 {
 	int i, j , k; 
     // i = index of left array, j = index of right array, index of the array in which left and right subarrays are to be merged.
     i = j = k = 0;
-    //this condition is to make sure that the it iterates over the loop whenever the size of the indexes i and j are less than the maximum/total size of the subarray. (it's obvious, that index can't be higher than the actual size of array)
+    //this condition is to make sure that the it iterates over the loop whenever the size of the indexes i and j are 
+ //less than the maximum/total size of the subarray. (it's obvious, that index can't be higher than the actual size of array)
 	while (i < nl && j < nr) 
 	{
-        //If element at i index of left array is lesser or equal to the element at j index of right array, then it is pushed to the array a[] where we are merging the 2 arrays in sorted order.
+        //If element at i index of left array is lesser or equal to the element at j index of right array, then it is pushed 
+		//to the array a[] where we are merging the 2 arrays in sorted order.
         if (left[i] <= right[j]) 
 	    {
 		    a[k] = left[i];
@@ -47,11 +50,14 @@ void merge(int a[], int left[], int right[], int nl, int nr)
 		    a[k] = right[j];
 		    j++;
 	    }
-        //increasing index of the parent array. (since the index is to be increased by 1 after the value at a[k] has been inserted after the above comparisons.)
+        //increasing index of the parent array. (since the index is to be increased by 1 after the value at a[k] has been 
+		//inserted after the above comparisons.)
 		k = k + 1; 
 	}
     /*
-    The below 2 while loops are for when one of the arrays have had all of their elements inserted into the parent array a[], and the other array is still left with some elements, the comparisons above won't work for this case, hence we had to apply a different condition/ different way to insert the elements in the leftover array.
+    The below 2 while loops are for when one of the arrays have had all of their elements inserted into the parent array a[], and 
+	the other array is still left with some elements, the comparisons above won't work for this case, hence we had to apply a 
+	different condition/ different way to insert the elements in the leftover array.
     */
 	while (i < nl) 
 	{
